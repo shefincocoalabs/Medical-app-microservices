9b64f4838745bb9a4d85d5c3e8b50a352d2eb573
shefin <shefin@cocoalabs.in>
2020-03-03 11:37:14 +0530

    db config changes

--- config/database.config.js
+++ config/database.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   development: {     
-    url: 'mongodb+srv://admin:admin123@cluster0-trsnb.mongodb.net/Medical?retryWrites=true&w=majority'
+    url: 'mongodb+srv://admin:Cmx5tPxvRFCjB8F9@cluster0-trsnb.mongodb.net/Medical?retryWrites=true&w=majority'
   },
   qa: {
-    url: 'mongodb+srv://admin:admin123@cluster0-trsnb.mongodb.net/Medical?retryWrites=true&w=majority'
+    url: 'mongodb+srv://admin:Cmx5tPxvRFCjB8F9@cluster0-trsnb.mongodb.net/Medical?retryWrites=true&w=majority'
   }
 
 }
