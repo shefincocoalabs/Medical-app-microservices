ff95c976c462ff356ec648671de345721bab6843
Cocoalabs <rahul.cocoalabs.com>
2020-05-14 10:12:06 +0530

    change

--- config/database.config.js
+++ config/database.config.js
@@ -1,14 +1,14 @@
 module.exports = {
   development: {     
     // url: 'mongodb://@localhost:27017/Medical',
-    url: 'mongodb://qadba2020:qaxon%402020@localhost:27017/Medical',
+    url: 'mongodb://qadba2020:qaxon%402020@localhost:27017/qaxon',
 
     // user: 'qadba2020', 
     // pass: 'qaxon@2020'
   },
   production: {
     // url: 'mongodb://@localhost:27017/Medical',
-    url: 'mongodb://qadba2020:qaxon%402020.com@localhost:27017/Medical',
+    url: 'mongodb://qadba2020:qaxon%402020.com@localhost:27017/qaxon',
 
     // url: 'mongodb+srv://admin:Cmx5tPxvRFCjB8F9@cluster0-trsnb.mongodb.net/Medical?retryWrites=true&w=majority'
     // url: 'mongodb://127.0.0.1:27017',
