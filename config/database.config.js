95b8d72449f82083ad2c8f52266fb7196f823996
Cocoalabs <rahul.cocoalabs.com>
2020-05-14 07:51:39 +0530

    change

--- config/database.config.js
+++ config/database.config.js
@@ -6,7 +6,7 @@ module.exports = {
   },
   production: {
     // url: 'mongodb+srv://admin:Cmx5tPxvRFCjB8F9@cluster0-trsnb.mongodb.net/Medical?retryWrites=true&w=majority'
-    url: 'mongodb://@localhost:27017/Medical',
+    url: 'mongodb://qadba2020:qaxon%402020@localhost:27017/Medical',
     user: 'qadba2020', 
     pass: 'qaxon@2020'
 
