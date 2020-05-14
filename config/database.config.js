a8338cb2d4060061287a076fd8b5d60d0a256f11
Cocoalabs <rahul.cocoalabs.com>
2020-05-14 07:29:01 +0530

    change

--- config/database.config.js
+++ config/database.config.js
@@ -1,11 +1,16 @@
 module.exports = {
   development: {     
-    // url: 'mongodb+srv://admin:Cmx5tPxvRFCjB8F9@cluster0-trsnb.mongodb.net/Medical?retryWrites=true&w=majority'
-    url: 'mongodb://localhost:27017/Medical'
+    url: 'mongodb://@localhost:27017/Medical'
+    // user: 'qadba2020', 
+    // pass: 'qaxon@2020'
   },
   production: {
     // url: 'mongodb+srv://admin:Cmx5tPxvRFCjB8F9@cluster0-trsnb.mongodb.net/Medical?retryWrites=true&w=majority'
-    url: 'mongodb://qadba2020:qaxon%402020.com@localhost:27017/Medical'
+    url: 'mongodb://@localhost:27017/Medical',
+    user: 'qadba2020', 
+    pass: 'qaxon@2020'
+
+    // {user: '<USER_NAME>', pass: '<P@SS>'}
   }
 
 }
