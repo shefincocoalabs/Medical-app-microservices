c3f7807f7d9810fed3ef10ecb44d0770765962d7
Cocoalabs <rahul.cocoalabs.com>
2020-05-14 10:30:27 +0530

    change

--- config/database.config.js
+++ config/database.config.js
@@ -8,12 +8,12 @@ module.exports = {
   },
   production: {
     // url: 'mongodb://@localhost:27017/qaxon',
-    url: 'mongodb://qadba2020:qaxon%402020.com@127.0.0.1:27017/qaxon?authSource=qaxon',
+    url: 'mongodb://qadba2020:qaxon%402020.com@127.0.0.1:27017/qaxon?authSource=admin',
 
     // url: 'mongodb+srv://admin:Cmx5tPxvRFCjB8F9@cluster0-trsnb.mongodb.net/Medical?retryWrites=true&w=majority'
     // url: 'mongodb://127.0.0.1:27017',
-    user: 'qadba2020', 
-    pass: 'qaxon@2020',
+    // user: 'qadba2020', 
+    // pass: 'qaxon@2020',
     // authObj :{
     //   "auth": {
     //     "authSource": "admin"
