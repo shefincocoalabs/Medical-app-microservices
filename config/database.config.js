0cc1b7021f3dc1e5c9ed7559bdcf174884e4b57c
Cocoalabs <rahul.cocoalabs.com>
2020-05-14 07:00:34 +0530

    change

--- config/database.config.js
+++ config/database.config.js
@@ -3,9 +3,9 @@ module.exports = {
     // url: 'mongodb+srv://admin:Cmx5tPxvRFCjB8F9@cluster0-trsnb.mongodb.net/Medical?retryWrites=true&w=majority'
     url: 'mongodb://localhost:27017/Medical'
   },
-  qa: {
+  production: {
     // url: 'mongodb+srv://admin:Cmx5tPxvRFCjB8F9@cluster0-trsnb.mongodb.net/Medical?retryWrites=true&w=majority'
-    url: 'mongodb://localhost:27017/Medical'
+    url: 'mongodb://qadba2020:qaxon@2020.com@localhost:27017/qaxon'
   }
 
 }
