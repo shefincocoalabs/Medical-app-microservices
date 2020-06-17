c2dc0770efd45bff0130f88a718bb9e0c2400551
Cocoalabs <rahul.cocoalabs.com>
2020-06-17 15:41:46 +0530

    change

--- app/routes/videos.routes.js
+++ app/routes/videos.routes.js
@@ -6,6 +6,6 @@ module.exports = (app) => {
     app.get('/videos/home',auth, video.getHomeVideo);
     app.get('/videos/chapter/:chapterId',auth, video.getChapterVideo);
     app.get('/videos/next-videos/:chapterId',auth, video.nextVideos);
-    app.post('/videos/:videoId/mark-as-watched', video.markAsWatched);
+    app.post('/videos/:videoId/mark-as-watched',auth, video.markAsWatched);
 };
 
